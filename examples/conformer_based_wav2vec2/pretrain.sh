4b2626d1678a0f2dd2bb7cc85a8e98bba6d08dd9
lovemefan <450489712@qq.com>
2021-12-26 20:35:53 +0800

    model architecture

--- examples/conformer_based_wav2vec2/pretrain.sh
+++ examples/conformer_based_wav2vec2/pretrain.sh
@@ -1,7 +1,8 @@
 fairseq-hydra-train \
-  +task.data=/home/dataset/wenet_s/wav2vec_manifests \
-  common.tensorboard_logdir=/root/data/Semi-Supervised-Speech-Recognize/tensorboard/wenetspeech \
+  +task.data=/root/dataset/speechDataset/wenetspeech-s/wav2vec_manifests \
+  common.tensorboard_logdir=/root/data/Semi-Supervised-Speech-Recognize/tensorboard/wenetspeech/pretrain \
   distributed_training.distributed_world_size=1 \
+  checkpoint.save_dir=/root/data/Semi-Supervised-Speech-Recognize/chechpint/wenetspeech/pretrain \
   model._name=conformer_based_wav2vec2 \
   --config-dir   /root/data/Semi-Supervised-Speech-Recognize/examples/conformer_based_wav2vec2/config/pretraining \
   --config-name   conformer_based_wav2vec2
